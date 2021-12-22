b81b9fe2b21e9e7c00ad19f32226092c31b2a6fb
Ilya udalov <ilya_udalov_04@mail.ru>
2021-12-22 17:18:57 +0300

    update checking

--- main/__main__.py
+++ main/__main__.py
@@ -1,5 +1,11 @@
+import time
+
 from main.service.WildcardMatcher import WildcardMatcher
 
 if __name__ == '__main__':
     wildcard_matcher: WildcardMatcher = WildcardMatcher()
-    wildcard_matcher.is_match("cbcb", "c*b")
+    input_data = input("please, enter a string ")
+    pattern_data = input("please, enter a pattern ")
+    start_time = time.time()
+    wildcard_matcher.is_match(input_data, pattern_data)
+    print(f'{wildcard_matcher.result}\nSeconds {time.time() - start_time}')
--- main/service/WildcardMatcher.py
+++ main/service/WildcardMatcher.py
@@ -1,52 +1,52 @@
 class WildcardMatcher:
 
-    def is_match(self, data: str, pattern: str) -> bool:
+    def __init__(self) -> None:
+        super().__init__()
+        self.result: bool = True
+
+    def is_match(self, data: str, pattern: str):
+
         split_data: list = list(data)
         split_pattern: list = list(pattern)
+
         if '*' not in pattern:
+
             if len(split_data) == len(split_pattern):
-                count_of_single = split_pattern.count("?")
-                if count_of_single != 0:
-                    while count_of_single != 0:
-                        split_pattern[split_pattern.index("?")] = split_data[split_pattern.index("?")]
-                        count_of_single -= 1
-                identified_char = True
+
+                self.result = True
+
                 for index in range(len(split_pattern)):
-                    if split_pattern[index] != split_data[index]:
-                        identified_char = False
-                        break
-
-                print(identified_char)
-
+                    if split_pattern[index] != split_data[index] and split_pattern[index] != "?":
+                        self.result = False
+                        return
             else:
-                print(False)
-
+                self.result = False
         else:
+
             count_of_characters = split_pattern.count("*")
             while count_of_characters != 0:
-                identified_char = True
+                start_index = split_pattern.index('*') + 1
                 for index in range(split_pattern.index("*") - 1):
-                    if split_pattern[index] != split_data[index]:
-                        if split_pattern[index] != '?':
-                            identified_char = False
-                            break
-                if not identified_char:
-                    print(False)
-                    break
-                start_index = split_pattern.index("*")
-                if len(split_pattern) != 1 and split_pattern.index("*") != len(split_pattern) - 1 and split_pattern.index("*") + 1 != len(split_pattern) -1:
-                    while split_data[start_index] != split_pattern[split_pattern.index("*") + 1]:
-                        start_index += 1
-                        if start_index > len(split_data) - 1:
-                            print(False)
-                            count_of_characters -= 1
-                            break
-                        elif start_index == len(split_data) - 1:
-                            count_of_characters -= 1
-                            print(True)
-                            break
+
+                    if split_pattern[index] != split_data[index] and split_pattern[index] != '?':
+                        self.result = False
+                        return
+                if start_index == len(split_data) - 1:
+                    self.result = True
+                    return
                 else:
-                    print(True)
-                    break
+                    if split_pattern[split_pattern.index('*') + 1] == '?':
+                        count_of_characters -= 1
+                        self.result = True
+                    else:
+                        if split_data[start_index + 1] == split_pattern[split_pattern.index("*") + 1] or split_pattern[split_pattern.index("*") + 1] == '?':
+                            count_of_characters -= 1
+                            self.result = True
 
+                        while split_data[start_index] != split_pattern[split_pattern.index("*") + 1]:
 
+                            start_index += 1
+
+                            if start_index > len(split_data) - 1:
+                                self.result = False
+                                return
